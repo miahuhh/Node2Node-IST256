086dc8e483d8cc62f1137e701d51604c3966abe7
miah <137248832+miahuhh@users.noreply.github.com>
2025-04-10 06:57:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
+app.use(express.static('public'));
+
 //static file serving
 app.use(express.static(path.join(__dirname)));
 
